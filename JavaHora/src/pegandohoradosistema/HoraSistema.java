404a4a20dc5188ead6d519d1254f7f17c40dd134
Asael Honório <75455723+asaelhonorio@users.noreply.github.com>
2021-01-15 14:20:40 -0300

    Código para mostrar Hora e Data do sistema

---
+++ JavaHora/src/pegandohoradosistema/HoraSistema.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package pegandohoradosistema;
+
+import java.util.Date;
+
+public class HoraSistema {
+	public static void main(String[] args) {
+		Date hora = new Date();
+		System.out.print("A Data e hora atual: ");
+		System.out.println(hora.toString());
+		
+		
+	}
+
+}
